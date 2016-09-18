2f45a533a939329350bbabf1efb9577362febbf1
mail@magic-mouse.-net <mail@magic-mouse.-net>
2016-09-18 21:39:10 +0200

    Added developer to spring to improve productivity Fixed problem where words appeared twice

--- src/main/java/com/github/magicmouse/Controller/TwitterController.java
+++ src/main/java/com/github/magicmouse/Controller/TwitterController.java
@@ -2,6 +2,8 @@ package com.github.magicmouse.Controller;
 
 
 import com.github.magicmouse.Models.TwitterModel;
+import com.github.magicmouse.Models.Word;
+import com.github.magicmouse.helpers.WordCounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -26,16 +29,25 @@ public class TwitterController {
     }
 
     @RequestMapping("/twitter_cloud")
-    public List<String> twitterCloud(@RequestParam(value="q") String query,
-                                     @RequestParam(value="count", defaultValue = "10") int count) {
+    public List<Word> twitterCloud(@RequestParam(value="q") String query,
+                                   @RequestParam(value="count", defaultValue = "10") int count) {
         // String generics on ArrayList is defined because java wants it to make it secure.
-        List<String> tweetStrings = new ArrayList<String>();
+        List<String> tweetLineStrings = new ArrayList<String>();
+        List<String> tweetWordStrings = new ArrayList<String>();
         // Twitter spring API
         List<Tweet> tweets = twitter.searchOperations().search(query, count).getTweets();
+        // Moving tweets in to a list for further processing.
+        tweets.stream().forEach(tweet -> tweetLineStrings.add(tweet.getText()));
+        // Not really a readable line of code, might refactor it later.
+        tweetLineStrings.stream().forEach(tweStr -> tweetWordStrings.addAll(Arrays.asList(tweStr.split(" "))));
 
-        tweets.stream().forEach(tweet -> tweetStrings.add(tweet.getText()));
 
-        return tweetStrings;
+
+
+        WordCounter wordCounter = new WordCounter();
+        tweetWordStrings.stream().forEach(str -> wordCounter.countWord(str));
+        return wordCounter.getWordList();
+
     }
 
 }
