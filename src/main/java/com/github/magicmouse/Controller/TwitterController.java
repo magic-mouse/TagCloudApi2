8153eba54cb93287d58732b82a99c5562dbfcf3b
mail@magic-mouse.-net <mail@magic-mouse.-net>
2016-09-18 20:52:02 +0200

    First working from twitter, getting lines.

--- src/main/java/com/github/magicmouse/Controller/TwitterController.java
+++ src/main/java/com/github/magicmouse/Controller/TwitterController.java
@@ -2,15 +2,40 @@ package com.github.magicmouse.Controller;
 
 
 import com.github.magicmouse.Models.TwitterModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class TwitterController {
 
-    @RequestMapping("/api/twitter")
-    public TwitterModel greeting(@RequestParam(value="q") String query) {
-        return new TwitterModel();
+
+    @Autowired
+    Twitter twitter;
+
+    @RequestMapping("/twitter")
+    public List<Tweet> twitter(@RequestParam(value="q") String query) {
+        return twitter.searchOperations().search(query, 10).getTweets();
     }
+
+    @RequestMapping("/twitter_cloud")
+    public List<String> twitterCloud(@RequestParam(value="q") String query,
+                                     @RequestParam(value="count", defaultValue = "10") int count) {
+        // String generics on ArrayList is defined because java wants it to make it secure.
+        List<String> tweetStrings = new ArrayList<String>();
+        // Twitter spring API
+        List<Tweet> tweets = twitter.searchOperations().search(query, count).getTweets();
+
+        tweets.stream().forEach(tweet -> tweetStrings.add(tweet.getText()));
+
+        return tweetStrings;
+    }
+
 }
